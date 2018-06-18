ead0546f135b3643bd501bcb676992385f209c7d
Jaegar Sarauer <jaegars@hotmail.com>
2018-06-18 11:01:24 -0700

    Package location update.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: './src/entry.js',
+    entry: 'src/entry.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
